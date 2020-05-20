60bf8d1da3efb10dbc1440776139e2b37846272c
Sargita92 <rfamcastro92@hotmail.com>
2020-05-20 17:10:59 -0400

    new version

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 requireDir('./src/models');
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/', (req,res) => {
-    /*Product.create({
-        title: "React native",
-        description: "Desenvolvimento com react",
-        url: "http://github.com/facebook/react-native",
-    });
-*/
-    return res.send("teste!123");
-});
+const ProductController = require('./controllers/ProductController');
+
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
